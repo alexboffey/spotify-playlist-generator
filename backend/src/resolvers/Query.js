ed1275f39b7b46cbeb26194b499e97c1d6ba0d69
Alex Boffey <alex@alexboffey.co.uk>
2019-06-13 16:42:54 +0100

    Basic sign in / out flow

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,7 @@
-const { hasPermission } = require("./helpers")
-
 module.exports = {
   me(parent, args, ctx, info) {
     // Check if we have a userId on the request aka if a user is logged in
-    //  Return promise directly
+    // Return promise directly
     if (!ctx.request.userId) return null; // Return null as noone is logged in
     return ctx.database.query.user(
       {
@@ -14,9 +12,8 @@ module.exports = {
   },
   async users(parent, args, ctx, info) {
     // Check if logged in
-    // if (!ctx.request.userId) {
-    //   throw new Error("You must be logged in!");
-    // }
+    if (!ctx.request.userId) throw new Error("You must be logged in!");
+
     // If they do, run the query
     return ctx.database.query.users({}, info);
   }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { ApolloProvider, Query } from "react-apollo";
+import React, { useState } from "react";
+import { ApolloProvider, Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import apollo from "./lib/createApolloClient";
 import "./App.css";
 
-const USERS_QUERY = gql`
+const USER_QUERY = gql`
   query {
     me {
       id
@@ -15,21 +15,98 @@ const USERS_QUERY = gql`
   }
 `;
 
+const SIGN_IN_MUTATION = gql`
+  mutation SIGN_IN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const SIGN_OUT_MUTATION = gql`
+  mutation SIGN_OUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`;
+
 const App: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <ApolloProvider client={apollo}>
       <div className="App">
-        <Query query={USERS_QUERY}>
+        <Query query={USER_QUERY}>
           {({ loading, data }: { loading: boolean; data: any }) => {
             if (loading) return <p>Loading...</p>;
 
             return (
-              <pre>
-                <code>{JSON.stringify(data, null, 2)}</code>
-              </pre>
+              <React.Fragment>
+                <pre>
+                  <code>{JSON.stringify(data, null, 2)}</code>
+                </pre>
+
+                {data.me && (
+                  <Mutation
+                    mutation={SIGN_OUT_MUTATION}
+                    refetchQueries={[{ query: USER_QUERY }]}
+                  >
+                    {(signout: () => any) => (
+                      <div>
+                        <button onClick={() => signout()}>Sign Out</button>
+                      </div>
+                    )}
+                  </Mutation>
+                )}
+              </React.Fragment>
             );
           }}
         </Query>
+
+        <Mutation
+          mutation={SIGN_IN_MUTATION}
+          refetchQueries={[{ query: USER_QUERY }]}
+          variables={{ email, password }}
+        >
+          {(signin: () => any, { loading }: { loading: boolean }) => (
+            <form
+              method="post"
+              onSubmit={async e => {
+                e.preventDefault();
+                await signin();
+                setEmail("");
+                setPassword("");
+              }}
+            >
+              <h2>Sign In</h2>
+              <label>
+                Email
+                <br />
+                <input
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </label>
+              <br />
+              <label>
+                Password
+                <br />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </label>
+              <br />
+              <button type="submit">Log In</button>
+            </form>
+          )}
+        </Mutation>
       </div>
     </ApolloProvider>
   );
